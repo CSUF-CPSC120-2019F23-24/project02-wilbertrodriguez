e818a17d2c8c6ed3dff894dc724037a4144d6b07
wilbertrodriguez <wilbertrodriguez@csu.fullerton.edu>
2019-12-09 10:40:45 -0800

    finished

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // This program calculates and displays business expenses.
 #include <iostream>
 #include <iomanip>
+#include <sstream>
 //so I can use setw(#)
 
 int main()
@@ -9,14 +10,13 @@ int main()
   std::cout << "Welcome to the Business Trip Tracker!\n\n";
 
   int days;
-  double hotel_exp,meal_exp,total_exp;
+  double hotel_exp, meal_exp, total_exp;
   std::string location;
+  std::stringstream sshotel, ssmeal, sstotal;
   //sets up the variables
 
   std::cout << "What is the buisness trip location? ";
-  std::string str = location;
-  std::cout << str.substr (0,20) << std::endl;
-  std::cin >> location;
+  getline(std::cin, location);
   //obtains the location using a string type
 
   std::cout << "How many days will the trip take? ";
@@ -25,20 +25,26 @@ int main()
 
   std::cout << "What is the total hotel expense? ";
   std::cin >> hotel_exp;
+  sshotel << std::setprecision(2) << std::fixed << '$' << hotel_exp;
   //obtains the hotel expenses for the trip
 
   std::cout << "What is the total meal expense? ";
   std::cin >> meal_exp;
+  ssmeal << std::setprecision(2) << std::fixed << '$' << meal_exp;
   //obtains the meal expenses for the trip
 
-  std::cout << "Location" << std::setw(12) << "Days" << std::setw(13)
-  << "Hotel" << std::setw(12) << "Meal" << std::setw(13) << "Total\n";
+  std::cout << std::setw(20) << std::left << "Location" << std::setw(10) << std::right << "Days"
+  << std::setw(10) << "Hotel" << std::setw(10) << "Meal" << std::setw(10) << "Total" << "\n";
+
+
   //Puts the data into the desired format
 
   total_exp = hotel_exp + meal_exp;
+  sstotal << std::setprecision(2) << std::fixed << '$' << total_exp;
   //calculates the total expenses for the trip
 
-  std::cout << location << std::setw(14) << days << std::setw(7) << '$' << hotel_exp << std::setw(6) << '$' << meal_exp << std::setw(6) << '$' << total_exp <<"\n";
+  std::cout << std::setw(20) << std::left << location.substr(0,20) << std::setw(10) << std::right << days
+  << std::setw(10) << sshotel.str() << std::setw(10) << ssmeal.str() << std::setw(10) << sstotal.str() << "\n";
   //puts the data below the previous names returned
 
   return 0;
